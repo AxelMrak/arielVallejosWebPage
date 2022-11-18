445f779550b0a8cd4a05abb7957306f7731e5279
AxelMrak <axelmrak@gmail.com>
2022-11-18 17:31:21 -0300

    Add map

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from '../styles/LocationsSection.module.css';
+import { MapContainer } from './Map';
+
 
 function Locations() {
   return (
     <section className={styles.locationsMainContainer}>
         <h3>Encuéntranos</h3>
+        <MapContainer/>
     </section>
   )
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,3 +1,5 @@
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
 const markers=[{
                   lat: -34.8490263,
                   lng: -58.5187667
@@ -19,3 +21,28 @@ const markers=[{
                   lng: -58.495036
               }]
 
+
+
+// ...
+
+export class MapContainer extends Component {
+    render() {
+        return (
+            <Map google={this.props.google} zoom={14}>
+
+                <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
+            </Map>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: (AIzaSyDV9ulzKnNWo6 - e0LYKJCi4KiWOVWkrMYQ)
+})(MapContainer)
