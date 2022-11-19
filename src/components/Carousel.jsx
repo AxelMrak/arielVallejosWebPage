c1ddf078cee0e3afa66db90b3a5a22eb6034ec62
AxelMrak <axelmrak@gmail.com>
2022-11-19 13:32:58 -0300

    Added image and change height

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,28 +5,32 @@ import gif3 from '../assets/gifs/gif3.gif';
 import gif4 from '../assets/gifs/gif4.gif';
 import gif5 from '../assets/gifs/gif5.gif';
 import gif6 from '../assets/gifs/gif6.gif';
+import mainImage from '../assets/mainImg.jpeg';
 
 function CarouselComponet() {
   return (
     <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src={gif1} class="d-block w-100" alt="..."/>
+          <img src={gif1} class="d-block w-100" alt="..." height='400px'/>
         </div>
         <div className="carousel-item">
-          <img src={gif2} class="d-block w-100" alt="..."/>
+          <img src={gif2} class="d-block w-100" alt="..." height='400px' />
         </div>
         <div className="carousel-item">
-          <img src={gif3} class="d-block w-100" alt="..."/>
+          <img src={gif3} class="d-block w-100" alt="..." height='400px' />
         </div>
         <div className="carousel-item">
-          <img src={gif4} class="d-block w-100" alt="..." />
+          <img src={gif4} class="d-block w-100" alt="..." height='400px' />
         </div>
         <div className="carousel-item">
-          <img src={gif5} class="d-block w-100" alt="..." />
+          <img src={gif5} class="d-block w-100" alt="..." height='400px' />
         </div>
         <div className="carousel-item">
-          <img src={gif6} class="d-block w-100" alt="..." />
+          <img src={gif6} class="d-block w-100" alt="..." height='400px' />
+        </div>
+        <div className="carousel-item">
+          <img src={mainImage} class="d-block w-100" alt="..." height='400px' />
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
