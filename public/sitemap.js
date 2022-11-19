3f693cb8e4bd45a8cbb0e6880977223b09d3ed23
AxelMrak <axelmrak@gmail.com>
2022-11-19 20:20:55 -0300

    repair sitemap

--- public/sitemap.js
+++ public/sitemap.js
@@ -2,9 +2,9 @@ import GenerateSitemap from "react-router-sitemap-maker";
 import Routes from "../src/routes";
 
 const sitemapData = await GenerateSitemap(Routes(), {
-    baseUrl: "https://",
+    baseUrl: "https://terapiasariel.com/",
     hashrouting: true,
     changeFrequency: "monthly"
 });
 
-sitemapData.toFile("./dist/sitemap.xml");
+sitemapData.toFile("./public/sitemap.xml");
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,9 @@
 import { defineConfig } from 'vite';
-import  VitePluginSitemap  from 'vite-plugin-sitemap';
 import react from '@vitejs/plugin-react';
-
-const routes = [
-  { path: '/', name: 'Home' },
-  { path: '/terapias', name: 'Terapias' },
-  { path: '/contacto', name: 'Contacto' },
-];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePluginSitemap({
-      baseUrl: 'https://terapiasariel.com',
-      contentBase: 'public',
-      routes: routes,
-      urlGenHook: (config) => {
-        let updatedRoutes = config.routes;
-
-        /** put any logic here that you want to run before the sitemap.xml file is generated 
-         * ex: generate URLs for all of your blog posts
-         */
-
-        return updatedRoutes;
-      }
-    })
-  
+    react()
   ]
 })
