d25dc5ac7a08d9a9b7f61bcecad6ba2e1d2061a6
AxelMrak <axelmrak@gmail.com>
2022-11-19 13:18:48 -0300

    Carousel Added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
 import gif1 from '../assets/gifs/gif1.gif';
 import gif2 from '../assets/gifs/gif2.gif';
 import gif3 from '../assets/gifs/gif3.gif';
@@ -10,45 +9,36 @@ import styles from '../styles/carousel.module.css';
 
 function CarouselComponet() {
   return (
-      <Carousel className={styles.carouselContainer}>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={gif1} class="d-block w-100 h-300" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={gif2} class="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={gif3} class="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src={gif4} class="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={gif5} class="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={gif6} class="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
   )
 }
 
