f547a48780cdcf727e83ab567972427b77ee5b1e
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2023-01-09 13:18:43 -0300

    Change video to img

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -12,25 +12,25 @@ function CarouselComponet() {
     <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel" style={{ maxWidth: '400px' }}>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <video src={gif1} className="d-block w-80" alt="Columna vertebral gif" height='400px'/>
+          <img src={gif1} className="d-block w-80" alt="Columna vertebral gif" height='400px'/>
         </div>
         <div className="carousel-item">
-          <video src={gif2} className="d-block w-80" alt="Columna vertebral gif version 2" height='400px' />
+          <img src={gif2} className="d-block w-80" alt="Columna vertebral gif version 2" height='400px' />
         </div>
         <div className="carousel-item">
-          <video src={gif3} className="d-block w-80" alt="Columna vertebral parte superior gif" height='400px' />
+          <img src={gif3} className="d-block w-80" alt="Columna vertebral parte superior gif" height='400px' />
         </div>
         <div className="carousel-item">
-          <video src={gif4} className="d-block w-80" alt="Axis hueso gif" height='400px' />
+          <img src={gif4} className="d-block w-80" alt="Axis hueso gif" height='400px' />
         </div>
         <div className="carousel-item">
-          <video src={gif5} className="d-block w-80" alt="Axis hueso gif 2" width='320px' height='400px' />
+          <img src={gif5} className="d-block w-80" alt="Axis hueso gif 2" width='320px' height='400px' />
         </div>
         <div className="carousel-item">
-          <video src={gif6} className="d-block w-80" alt="Quiropraxia gif" width='320px' height='400px' />
+          <img src={gif6} className="d-block w-80" alt="Quiropraxia gif" width='320px' height='400px' />
         </div>
         <div className="carousel-item">
-          <video src={mainImage} className="d-block w-80" alt="Diferentes terapias y alimentacion imagen principal" height='400px' />
+          <img src={mainImage} className="d-block w-80" alt="Diferentes terapias y alimentacion imagen principal" height='400px' />
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
