b22a4144d60094a500a5922048eaaef1818a4d28
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2023-01-09 20:45:44 -0300

    Change in Main Section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,15 @@
 import img from "../assets/mainImg.jpeg";
 import styles from "../styles/MainSection.module.scss";
-import CarouselComponent from "./Carousel";
+import videoWebm from '../assets/video/video-main-desktop.webm';
+import videoMp4 from '../assets/video/video-main-desktop.mp4';
 
 function MainSection() {
 	return (
 		<main className={styles.mainContainer}>
-			<CarouselComponent/>
+			<video muted autoPlay preload="auto" loop >
+				<source src={videoMp4} type='video/mp4' />
+				<source src={videoWebm} type='video/webm'/>
+			</video>
 			<div className={styles.textMain}>
 				<h1>Terapias Holísticas <hr/> y <hr/> Alternativas </h1>
 			</div>
